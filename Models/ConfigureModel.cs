0d852d2130d9d0dd6ca351b3621bab54e712c9b6
jersonb <jersonlbrito@hotmail.com>
2022-09-26 21:01:40 -0300

    add mvp

---
+++ Models/ConfigureModel.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using QuestPDF.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cleo.Models
+{
+    public class ConfigureModel
+    {
+        [Display(Name = "Imagem")]
+        public IFormFile File { get; set; } = default!;
+
+        [Display(Name = "Nome de teste")]
+        public string Name { get; set; } = default!;
+
+        [Display(Name = "Nomes")]
+        public string Names { get; set; } = default!;
+
+        [Display(Name = "Nome da fonte")]
+        public string FontFamily { get; set; } = Fonts.TimesNewRoman!;
+
+        [Display(Name = "Tamanho da fonte")]
+        public int FontSize { get; set; } = 25;
+
+        [Range(0, 560)]
+        [Display(Name = "Posição do vertical do nome")]
+        public int PositionY { get; set; } = 230;
+
+        [Display(Name = "Em itálico")]
+        public bool Itailc { get; set; }
+
+        [Display(Name = "Em negrito")]
+        public bool Bold { get; set; }
+
+        [Display(Name = "Normalize")]
+        public bool Captalize { get; set; }
+
+        [Display(Name = "Caixa Alta")]
+        public bool ToUpper { get; set; }
+
+        public byte[] Data { get; set; } = Array.Empty<byte>();
+
+        public static IEnumerable<SelectListItem> FontNames => new List<SelectListItem>()
+    {
+        new SelectListItem(Fonts.Arial,Fonts.Arial),
+        new SelectListItem(Fonts.Calibri,Fonts.Calibri),
+        new SelectListItem(Fonts.LucidaConsole,Fonts.LucidaConsole),
+        new SelectListItem(Fonts.TimesNewRoman,Fonts.TimesNewRoman),
+        new SelectListItem(Fonts.Verdana,Fonts.Verdana),
+    };
+    }
+}
